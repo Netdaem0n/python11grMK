86d15caae886c4afd2b5f9aff7524a8c12189724
netdaemon <bakalets@gmail.com>
2022-10-11 12:31:39 +0500

    -

---
+++ 17-1.py
@@ -0,0 +1,17 @@
+# F(0) = 0;
+# F(n) = F(n − 1) + 1, если n > 0 и при этом n mod 3 = 2;
+# F(n) = F((n − n mod 3) / 3), если n > 0 и
+# при этом n mod 3 < 2.
+
+def F(n):
+    if n == 0:
+        return 0
+    if n > 0 and n % 3 == 2:
+        return F(n - 1) + 1
+    if n > 0 and (n % 3) < 2:
+        return F((n - n % 3) / 3)
+
+for i in range(0, 1000):
+    if F(i) == 5:
+        print(i)
+        break
